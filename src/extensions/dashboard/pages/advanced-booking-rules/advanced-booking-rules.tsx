f5924255e37adf7fec3bdd8a2c27a61f20b19e8d
Mickalive <mickaelvuilleumier@gmail.com>
2026-09-03 01:04:06 +0200

    finalization: mount real rules editor in Wix page

--- src/extensions/dashboard/pages/advanced-booking-rules/advanced-booking-rules.tsx
+++ src/extensions/dashboard/pages/advanced-booking-rules/advanced-booking-rules.tsx
@@ -1,25 +1,6 @@
 import type { FC } from 'react';
-import { EmptyState, Page, WixDesignSystemProvider } from '@wix/design-system';
-import '@wix/design-system/styles.global.css';
+import AdvancedRulesApp from '../../runtime/AdvancedRulesApp';
 
-const DashboardPage: FC = () => {
-  return (
-    <WixDesignSystemProvider>
-      <Page>
-        <Page.Header
-          title="Advanced Booking Rules"
-          subtitle="This is a subtitle for your page"
-        />
-        <Page.Content>
-          <EmptyState
-            title="Advanced Booking Rules"
-            subtitle="Edit your page code to change this text."
-            skin="page"
-          />
-        </Page.Content>
-      </Page>
-    </WixDesignSystemProvider>
-  );
-};
+const DashboardPage: FC = () => <AdvancedRulesApp />;
 
 export default DashboardPage;

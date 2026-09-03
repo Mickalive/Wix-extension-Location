948519cd5c0e9710ef94b71d866cca7eb8cb4f7f
Mickalive <mickaelvuilleumier@gmail.com>
2026-09-03 02:58:29 +0200

    runtime: remove artificial collection env dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig, envField } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import wix from '@wix/astro';
 import react from '@astrojs/react';
 import wixHostingAdapter from '@wix/astro-wix-hosting-adapter';
@@ -11,9 +11,4 @@ export default defineConfig({
   image: { domains: ['static.wixstatic.com'] },
   security: { checkOrigin: false },
   devToolbar: { enabled: false },
-  env: {
-    schema: {
-      ABR_COLLECTION_ID: envField.string({ context: 'server', access: 'public' }),
-    },
-  },
 });

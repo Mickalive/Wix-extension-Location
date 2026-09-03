b11d98a3d0b096eed77ecc40e656417183e3a685
Mickalive <mickaelvuilleumier@gmail.com>
2026-09-03 02:53:53 +0200

    runtime: declare server collection id

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import wix from '@wix/astro';
-import react from "@astrojs/react";
-import wixHostingAdapter from "@wix/astro-wix-hosting-adapter";
+import react from '@astrojs/react';
+import wixHostingAdapter from '@wix/astro-wix-hosting-adapter';
 
 export default defineConfig({
-  output: "server",
+  output: 'server',
   adapter: wixHostingAdapter(),
   integrations: [wix(), react()],
-  image: { domains: ["static.wixstatic.com"] },
+  image: { domains: ['static.wixstatic.com'] },
   security: { checkOrigin: false },
-  devToolbar: { enabled: false }
+  devToolbar: { enabled: false },
+  env: {
+    schema: {
+      ABR_COLLECTION_ID: envField.string({ context: 'server', access: 'public' }),
+    },
+  },
 });
